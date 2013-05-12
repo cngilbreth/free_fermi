be2440b1a8ef430db6ae70794bc1ebed3652a91e
Christopher N. Gilbreth <cngilbreth@gmail.com>
2013-05-12 11:06:00 -0400

    Update test data

--- test/run.sh
+++ test/run.sh
@@ -5,37 +5,37 @@ PROG=../src/free_fermi
 betas=`seq 1.0 1.0 10.0`
 echo $betas
 
-echo "#beta       H"
+echo "#beta       E"
 for beta in $betas; do
     echo -n "$beta   "
-    $PROG H 10 10 $beta
+    $PROG E 10 $beta
 done
 
 echo ""
 echo "#beta       C"
 for beta in $betas; do
     echo -n "$beta   "
-    $PROG C 10 10 $beta
+    $PROG C 10 $beta
 done
 
 echo ""
 echo "#beta       F"
 for beta in $betas; do
     echo -n "$beta   "
-    $PROG F 10 10 $beta
+    $PROG F 10 $beta
 done
 
 echo ""
-echo "#beta     H_spin"
+echo "#beta     E_spin"
 for beta in $betas; do
     echo -n "$beta   "
-    $PROG H_spin 10 10 $beta
+    $PROG E_spin 10 $beta
 done
 
 echo ""
 echo "#beta     F_spin"
 for beta in $betas; do
     echo -n "$beta   "
-    $PROG F_spin 10 10 $beta
+    $PROG F_spin 10 $beta
 done
 
